e27c49c6f74caf959791271e0d259eec9e0d4662
Stephen Selorm Bedzrah <49565920+CoderLinesTechnology@users.noreply.github.com>
2025-04-07 09:15:59 +0000

    Company Inteface show

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,7 @@
 import { Navigate } from 'react-router-dom';
 
 const Index = () => {
-  return <Navigate to="/login" replace />;
+  return <Navigate to="/company/dashboard" replace />;
 };
 
 export default Index;
